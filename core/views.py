d377199e43653f0628d315dd0a77bd582ed045d0
Pierre TInard <pierre@tinard.fr>
2019-10-22 12:00:15 +0200

    init

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Link
 
 # Create your views here.
 
@@ -6,4 +7,14 @@ from django.shortcuts import render
 def home(request):
     home_nav = "active"
     # context = {"home_nav": "active"}
+    form = LinkAdd(request.POST or None)
+    if form.is_valid():
+        error = False
+        user_data = form.cleaned_data["nom"]
+
+        if Link.objects.filter(name=user_data).exists():
+            error = True
+        else:
+            marque = Link(name=user_data).save()
+
     return render(request, "core/pages/home.html", locals())
