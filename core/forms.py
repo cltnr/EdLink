8f01c7a28037f74d7f2bbc08b03b7fd0c72547ae
Pierre TInard <pierre@tinard.fr>
2019-11-23 20:56:12 +0100

    layout

--- core/forms.py
+++ core/forms.py
@@ -2,14 +2,16 @@ from django import forms
 
 
 class LinkAdd(forms.Form):
-    link = forms.CharField(
-        min_length=2,
-        max_length=15,
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Lien raccourci..."})
-    )
-
     url = forms.CharField(
         min_length=3,
         max_length=200,
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "URL"})
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "en.wikipedia.org/wiki/Saturn"})
     )
+
+    link = forms.CharField(
+        min_length=2,
+        max_length=15,
+
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "wiki_saturn"})
+    )
+
--- core/views.py
+++ core/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from .models import Link
 from .forms import LinkAdd
+from django.contrib.sites.shortcuts import get_current_site
+
 
 # Create your views here.
 
 
 def home(request):
     home_nav = "active"
+
     # context = {"home_nav": "active"}
+    app_url = get_current_site(request).domain
+    print(app_url)
     form = LinkAdd(request.POST or None)
     if form.is_valid():
         error = False
         user_data = form.cleaned_data["nom"]
-
         if Link.objects.filter(name=user_data).exists():
             error = True
         else:
