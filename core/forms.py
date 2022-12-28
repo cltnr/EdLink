5ea6cceb0737a6853aadefd13be0d16e9ef6fb73
Pierre Tinard <pierre@tinard.fr>
2022-12-28 23:04:22 +0100

    change stats url

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import RegexValidator
 
 
 class LinkAdd(forms.Form):
@@ -6,14 +7,24 @@ class LinkAdd(forms.Form):
         min_length=3,
         max_length=200,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "en.wikipedia.org/wiki/Saturn"}
+            attrs={
+                "class": "form-control",
+                "placeholder": "en.wikipedia.org/wiki/Saturn",
+            }
         ),
     )
 
     link = forms.CharField(
         min_length=2,
-        max_length=15,
+        max_length=20,
         required=False,
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "saturn"}),
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "saturn"}
+        ),
+        validators=[
+            RegexValidator(
+                "[a-zA-Z0-9]",
+                message="Short link must use 2 to 15 alphanumerical characters",
+            )
+        ],
     )
-
